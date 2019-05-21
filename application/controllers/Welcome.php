da4f0dcf9258bc34225ae5afbd936ce19314c96d
joshuanatan <joshuanatan.jn@gmail.com>
2019-05-21 08:44:42 +0700

    gitignore

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,9 @@
+<?php
+class Welcome extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function index(){
+        $this->load->view("landing/index");
+    }
+}
