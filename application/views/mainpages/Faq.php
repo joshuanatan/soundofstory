ff0d7adb599d2a08308ac4f17c97453b73033c42
wivinadaicy <43501017+wivinadaicy@users.noreply.github.com>
2019-06-22 20:44:01 +0700

    a

--- application/views/mainpages/Faq.php
+++ application/views/mainpages/Faq.php
@@ -1,1 +1,32 @@
- 
+<?php
+if($this->session->userdata("id_user") == "") {
+    redirect("welcome");
+}
+?>
+<div class="cart_info">
+    <div class="container">
+        <div class = "row" style = "margin-bottom:50px">
+            <div class = "col">
+            </div>
+        </div>
+        <br/><br/>
+        <div class = "row">
+            <table class = "table table-stripped">
+                <thead>
+                    <tr>
+                        <th style = "width:45%">Question</th>
+                        <th style = "width:45%">Answer</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($faq as $list) { ?>
+                    <tr>
+                        <Td class = "align-middle"><h4><?php echo $list->question;?></h4></Td>
+                        <Td class = "align-middle"><h4><?php echo $list->answer;?></h4></Td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>		
+</div>
