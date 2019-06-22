337bf58d9826897556022043442a2d91e80264d9
wivinadaicy <43501017+wivinadaicy@users.noreply.github.com>
2019-06-22 19:45:31 +0700

    asf

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -51,82 +51,68 @@
 		<section class="banner-area relative">
 			<div class="container">
 				<div class="row fullscreen align-items-center justify-content-center">
-					<div class="banner-left col-lg-6">
-						<img  class="d-flex mx-auto img-fluid" src="<?php echo base_url();?>assets/images/logosos.png" alt="">
+					<div class="banner-left col-lg-12">
+						<img  class="d-flex mx-auto img-fluid" src="<?php echo base_url();?>assets/images/logosos.png" width="40%" alt="">
 					</div>
-					<div class="col-lg-6">
-						<div class="story-content">
-							<h1>Reading stories <span class="sp-1">IS FUN</span><br>
-							Listening Stories <span class="sp-1">IS PRODUCTIVE</span></h1>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section class="feature-area pt-100 pb-100  relative">
-			<div class="overlay" style = "background-color:#5ce1e6"></div>
-			<div class="container">
-				<div class="row">
-				<?php foreach($episode as $list){ ?>
-					<div class="col-lg-4 col-sm-6">
-						<div class="single-feature">
-							<div class="icon" data-toggle="modal" data-target="#songs<?php echo $list->id_recording;?>">
-								<span class="lnr fa fa-play"></span>
-							</div>
-							<div class="desc">
-								<h2 class="text-uppercase"><?php echo $list->nama_playlist;?></h2>
-								<h5><?php echo $list->description_playlist;?></h5>
-								<hr/>
-								<h2><?php echo $list->description_recording;?></h2><p class="text-uppercase"><?php echo "Chapter ".$list->chapter_playlist;?></p>
-							</div>
-						</div>
-					</div>
-					<div class="modal fade" id="songs<?php echo $list->id_recording;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-						<div class="modal-dialog modal-dialog-centered" role="document">
-							<div class="modal-content">
-								
-								<div class="modal-body">
-									
-									<div class="row">
-										<div class="col">
-											<div class="home_content">
-												<div class="home_title"><strong><?php echo $list->nama_playlist ?><span>.</span></strong></div>
-												<div class="home_text"><p><?php echo $list->description_playlist ?></p></div>
-											</div>
+					<div class="col-lg-12 feature-area">
+						<div class="container">
+							<div class="row">
+							<?php foreach($episode as $list){ ?>
+								<div class="col-lg-4 col-sm-6">
+									<div class="single-feature">
+										<div class="icon" data-toggle="modal" data-target="#songs<?php echo $list->id_recording;?>">
+											<span class="lnr fa fa-play"></span>
 										</div>
-									</div>	
-									<div class="row">
-										<div class="col-lg-12">
-											<div class="details_content">
-												<div class="details_name"><strong>Chapter <?php echo $list->chapter_playlist ?></strong></div>
-												<div class="details_text">
-													<p><?php echo $list->description_recording ?></p>
-												</div>
-												<!-- In Stock -->
-												<div class="in_stock_container">
-													<div class="availability"><strong>Duration:</strong></div>
-													<span><?php echo $list->duration_recording.' minutes' ?></span>
+									</div>
+								</div>
+								<div class="modal fade" id="songs<?php echo $list->id_recording;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											
+											<div class="modal-body">
+												
+												<div class="row">
+													<div class="col">
+														<div class="home_content">
+															<div class="home_title"><strong><?php echo $list->nama_playlist ?><span>.</span></strong></div>
+															<div class="home_text"><p><?php echo $list->description_playlist ?></p></div>
+														</div>
+													</div>
+												</div>	
+												<div class="row">
+													<div class="col-lg-12">
+														<div class="details_content">
+															<div class="details_name"><strong>Chapter <?php echo $list->chapter_playlist ?></strong></div>
+															<div class="details_text">
+																<p><?php echo $list->description_recording ?></p>
+															</div>
+															<!-- In Stock -->
+															<div class="in_stock_container">
+																<div class="availability"><strong>Duration:</strong></div>
+																<span><?php echo $list->duration_recording.' minutes' ?></span>
+															</div>
+															
+
+															<!-- Share -->
+															<div style = "margin-top:20px">
+																<audio controls style = "width: 100%">
+																	<source src="<?php echo base_url().'assets/audio/'.$list->file_recording ?>" type="audio/mpeg">
+																</audio>
+															</div>
+														</div>
+													</div>
 												</div>
 												
-
-												<!-- Share -->
-												<div style = "margin-top:20px">
-													<audio controls style = "width: 100%">
-														<source src="<?php echo base_url().'assets/audio/'.$list->file_recording ?>" type="audio/mpeg">
-													</audio>
-												</div>
 											</div>
 										</div>
 									</div>
-									
 								</div>
+								<?php
+								}
+								?>
 							</div>
-						</div>
+				</div>
 					</div>
-					<?php
-					}
-					?>
-					
 				</div>
 			</div>
 		</section>
